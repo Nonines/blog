582c503d1fcbc0601c68ad10b1edc136476b6835
Banky <kingmorty@protonmail.com>
2024-07-19 20:10:29 +0100

    Trying docker deployment

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Caching events..."
+php artisan event:cache
+
+echo "Caching views..."
+php artisan view:cache
+
+echo "Running migrations..."
+php artisan migrate:fresh --seed
