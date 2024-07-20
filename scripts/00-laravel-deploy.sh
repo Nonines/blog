d519bddf7513ca16fc5b2167de834c3ef03943f8
Banky <kingmorty@protonmail.com>
2024-07-20 12:04:16 +0100

    Another update

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 echo "Running composer"
+cp /etc/secrets/.env .env
 composer global require hirak/prestissimo
-# composer install --no-dev --working-dir=/var/www/html
 composer install --working-dir=/var/www/html
 
 # echo "generating application key..."
 # php artisan key:generate --show
+
+echo "Clearing caches..."
+php artisan optimize:clear
 
 echo "Caching config..."
 php artisan config:cache
@@ -21,3 +24,5 @@ php artisan view:cache
 
 echo "Running migrations..."
 php artisan migrate:fresh --force --seed
+
+echo "done deploying"
